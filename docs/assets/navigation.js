ce22409b9f4fd2bbee454da7d4a03bafa6bafb69
Kshitij <kshitij.deshpande7@gmail.com>
2024-11-16 11:11:18 -0800

    Using better docs to generate docs for JS + TS files

--- docs/assets/navigation.js
+++
@@ -1,1 +0,0 @@
-window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslJKTC7JLEssSVXSUSpILMlQslJKK81LLsnMzyvWh8npZZTk5ijpKGVn5qUoWZmZ1OrAtaek4jMAIYthRCwAyQPEiIMAAAA="
