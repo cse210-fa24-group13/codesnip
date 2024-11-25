3028a4266a061f82e948fab7d41e1c5dc5fe67c8
YingJiehXia <156238108+YingJiehXia@users.noreply.github.com>
2024-11-25 00:16:48 -0800

    Delete out directory

--- out/data/dataAccess.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DataAccessConsts = void 0;
-class DataAccessConsts {
-}
-exports.DataAccessConsts = DataAccessConsts;
-DataAccessConsts.defaultRootElement = { id: 1, parentId: -1, label: 'snippets', lastId: 1, folder: true, children: [] };
-//# sourceMappingURL=dataAccess.js.map
